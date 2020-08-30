bea0da2801c584c168dce162711fc14642439c88
Christopher Arias <chrstphr.aaron@gmail.com>
2020-08-30 01:28:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,31 @@
+# class Song
+#
+#   attr_accessor :name, :artist
+#
+#   def initialize(name)
+#     @name = name
+#   end
+#
+#   def self.new_by_filename(filename)
+#     split_name = filename.split(" - ")
+#     song = self.new(split_name[1])
+#     artist = Artist.self.find_or_create_by_name(split_name[0])
+#     song.artist = artist
+#     artist.add_song(song)
+#     song
+#   end
+#
+#
+#   # def import
+#   #   self.files.each do |file|
+#   #     Song.new_by_filename(file)
+#   #     Artist.all << song.artist unless Artist.all.include?(song.artist)
+#   #   end
+#   # end
+#
+#
+# end
+
 class Song
 
   attr_accessor :name, :artist
@@ -6,22 +34,17 @@ class Song
     @name = name
   end
 
-  def self.new_by_filename(filename)
-    split_name = filename.split(" - ")
-    song = self.new(split_name[1])
-    artist = Artist.self.find_or_create_by_name(split_name[0])
-    song.artist = artist
-    artist.add_song(song)
-    song
+  def self.new_by_filename(file_name)
+    song = file_name.split(" - ")[1]
+    artist = file_name.split(" - ")[0]
+    new_song = self.new(song)
+    new_song.artist_name = artist
+    new_song
   end
 
-  
-  # def import
-  #   self.files.each do |file|
-  #     Song.new_by_filename(file)
-  #     Artist.all << song.artist unless Artist.all.include?(song.artist)
-  #   end
-  # end
-
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
 
 end
