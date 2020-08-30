cdd221e6164d51e582bd1827cfba0d9b1862843e
Christopher Arias <chrstphr.aaron@gmail.com>
2020-08-30 01:23:18 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,16 +1,34 @@
+# class MP3Importer
+#
+# attr_accessor :path
+#   def initialize(path)
+#     @path = path
+#   end
+#
+#   def files
+#     @files = Dir.entries(@path)
+#     @files.delete_if {|file| file == "." || file == ".."}
+#   end
+#
+#   def import
+#
+#   end
+# end
+
 class MP3Importer
 
-attr_accessor :path
+  attr_accessor :path
+
   def initialize(path)
     @path = path
   end
 
   def files
-    @files = Dir.entries(@path)
-    @files.delete_if {|file| file == "." || file == ".."}
+    @files ||= Dir.entries(@path).select {|song| !File.directory?(song) && song.end_with?(".mp3")}
   end
 
   def import
-
+    files.each {|song| Song.new_by_filename(song)}
   end
+
 end
