8c4f9c776babc50131b08421c05f98963d05be02
Christopher Arias <chrstphr.aaron@gmail.com>
2020-08-30 01:38:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,6 +34,10 @@ class Song
     @name = name
   end
 
+  def self.all
+    @@all
+  end
+
   def self.new_by_filename(file_name)
     song = file_name.split(" - ")[1]
     artist = file_name.split(" - ")[0]
