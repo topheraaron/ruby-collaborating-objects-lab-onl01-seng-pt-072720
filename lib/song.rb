ee125dcd90218a45f87e5d7fa5e4a7ec844242a0
Christopher Arias <chrstphr.aaron@gmail.com>
2020-08-30 01:43:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,10 +30,16 @@ class Song
 
   attr_accessor :name, :artist
 
+  @@all = []
+
   def initialize(name)
     @name = name
   end
 
+  def save
+    @@all << self
+  end
+  
   def self.all
     @@all
   end
